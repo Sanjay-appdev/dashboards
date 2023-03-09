3fd8ea133139c3d5a0877dc48ffa9bb3001cc087
sanjay5649 <stuwrkr-smaru7@mx.uillinois.edu>
2023-03-09 04:43:54 +0000

    Dashboard done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get("/forex", {:controller =>"currencies",:action =>"first_currency" })
 
   get("/forex/:from_currency",{ :controller => "currencies", :action =>"second_currency" })
+
+  get("/forex/:from_currency/:to_currency",{ :controller =>"currencies",:action =>"thrid_currency"})
   
 end
